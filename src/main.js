ce9ac3b5bc46a05c4e9d434b0355533ba8b19191
Jobith M Basheer <jobith@three38inc.com>
2020-09-14 19:37:52 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import webcam from "./webcam.vue";
 
-createApp(App).mount('#app')
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("v-webcam", webcam);
+}
+
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+webcam.install = install;
+
+export default webcam;
